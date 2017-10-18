9a30389569566ab7f41f7bd776503af56e187561
Dylan Randle <jacquielabatt@Dylans-MacBook.local>
2017-10-17 20:55:26 -0400

    add scrape code

---
+++ scrape.py
@@ -0,0 +1,52 @@
+import asyncio
+import aiohttp
+import bs4
+
+# some subreddits I'm interested in
+subreddits = ['/r/Science',
+    '/r/AskScience',
+    '/r/cogsci',
+    '/r/medicine',
+    '/r/everythingscience',
+    '/r/learnprogramming',
+    '/r/compsci',
+    '/r/javascript',
+    '/r/coding',
+    '/r/machinelearning',
+    '/r/howtohack',
+    '/r/cpp',
+    '/r/python',
+    '/r/learnpython',
+    '/r/engineering',
+    '/r/ubuntu',
+    '/r/EngineeringStudents',
+    '/r/askengineers',
+    '/r/stocks',
+    '/r/stockmarket',
+    '/r/math',
+    '/r/Space',
+    '/r/nhl',
+    '/r/spacex',
+    '/r/nasa',
+    '/r/history']
+
+async def get_top_link(page):
+    soup = bs4.BeautifulSoup(page, "lxml")
+    try:
+        return soup.find('div', id='siteTable').find('div', {"data-rank" : "1"})['data-permalink']
+    except:
+        return ''
+
+async def scrape(subreddit):
+    url = 'https://www.reddit.com'+subreddit
+    print('Starting {}'.format(subreddit))
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            page = await response.text()
+            top = await get_top_link(page)
+            print('Top post for {}: {}'.format(subreddit, 'https://www.reddit.com'+top)); return
+
+futures = [scrape(subreddit) for subreddit in subreddits]
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.wait(futures))
